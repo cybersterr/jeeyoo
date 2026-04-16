2f69b033f444a6e0a356ba6f7ea9c3e9c9102ee9
cybersterr <rupanidhruv244@gmail.com>
2026-04-16 23:58:14 +0530

    Update index.js

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 
 const INPUT_URL =
-  "https://raw.githubusercontent.com/cybersterr/jeeyo/refs/heads/main/output.json";
+  "https://raw.githubusercontent.com/cybersterr/jeeyo/refs/heads/main/stream.json";
 
 const OUTPUT_FILE = "output.json";
 
